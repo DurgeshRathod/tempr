68c2be71aed3726392eca28ee013356cc8c7a851
mo-mann <mo_mann@hotmail.com>
2018-12-22 16:54:10 +0530

    API's accessing environment variables.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 console.log('Testing...')
+console.log(process.env.MONGO_DB);
 
 const express = require('express'),
     app = express(),
@@ -8,24 +9,26 @@ const express = require('express'),
 app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hellp Express 2')
+    res.send('Hello Express 2')
 });
 
 app.get('/friends/:userid', (req, res) => {
-    res.send('Hello friends GET : ' + req.params.userid)
+    res.send('Hello friends GET : ' + req.params.userid + '. MongoDB Environment Variable : ' + process.env.MONGO_DB);
 });
 
 app.get('/friendsuggestions/:userid', (req, res) => {
-    res.send('Hello friendsuggestions GET : ' + req.params.userid)
+    res.send('Hello friendsuggestions GET : ' + req.params.userid + '. MongoDB Environment Variable : ' + process.env.MONGO_DB);
 });
 
 app.post('/friends/:userid', (req, res) => {
-    res.send('Hello friends POST : userid:' +  req.params.userid + '. FriendId : ' + req.body.friendid);
+    res.send('Hello friends POST : userid:' +  req.params.userid + '. FriendId : ' + req.body.friendid + '. MongoDB Environment Variable : ' + process.env.MONGO_DB);
 });
 
 app.delete('/friends/:userid', (req, res) => {
-    res.send('Hello friends DELETE : userid:' +  req.params.userid + '. FriendId : ' + req.body.friendid);
+    res.send('Hello friends DELETE : userid:' +  req.params.userid + '. FriendId : ' + req.body.friendid + '. MongoDB Environment Variable : ' + process.env.MONGO_DB);
 });
+
+
 
 app.listen(process.env.PORT || 3000)
 
