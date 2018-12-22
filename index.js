b621ae987cb37f4c0ab15b57b1769b4cc5131d04
Durgesh Rathod <durgeshrathod.777@gmail.com>
2018-12-22 17:35:09 +0530

    Added friends get and post

--- index.js
+++ index.js
@@ -1,35 +1,88 @@
 
-console.log('Testing...')
-console.log(process.env.MONGO_DB);
+var MongoClient = require('mongodb').MongoClient;
+const bodyParser =require('body-parser');
 
-const express = require('express'),
-    app = express(),
-    bodyparser = require('body-parser');
+var express = require('express');
+var app = express();
+var url = "mongodb://localhost:27017/friendsdb";
+var gDb;
+var gDbo;
 
-app.use(bodyparser.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello Express 2')
-});
-
-app.get('/friends/:userid', (req, res) => {
-    res.send('Hello friends GET : ' + req.params.userid + '. MongoDB Environment Variable : ' + process.env.MONGO_DB);
+app.get('/friends/:userId', (req, res) => {
+    let userId = parseInt(req.params.userId)
+    gDbo.collection("tblFriends").findOne({"userId": userId}, function(err, result) {
+        if (err) {
+            res.send('Friend get : ' + false + err);
+        }
+        console.log('fetching')
+        console.log(result.friends)
+        res.send(result.friends);
+    });
 });
 
 app.get('/friendsuggestions/:userid', (req, res) => {
     res.send('Hello friendsuggestions GET : ' + req.params.userid + '. MongoDB Environment Variable : ' + process.env.MONGO_DB);
 });
 
-app.post('/friends/:userid', (req, res) => {
-    res.send('Hello friends POST : userid:' +  req.params.userid + '. FriendId : ' + req.body.friendid + '. MongoDB Environment Variable : ' + process.env.MONGO_DB);
+app.post('/friends/:userId', (req, res) => {
+    let userId = parseInt(req.params.userId)
+    gDbo.collection("tblFriends").findOne({"userId": userId}, function(err, result) {
+        if (err) {
+            res.send("false");
+        }
+        console.log('fetching');
+        gDbo.collection("tblFriends").findOne({"userId": req.body.userid}, function(err, fr) {
+            if (err) {
+                res.send("false");
+            }
+            console.log('fetching')
+            
+            result.friends.push({userId: req.body.userid, name: fr.userName });
+            var myquery = { userId: userId };
+            var newvalues = { $set: { friends: result.friends } };
+            gDbo.collection("tblFriends").updateOne(myquery, newvalues, function(err, updateres) {
+                if (err) {
+                    res.send("false");
+                }
+                console.log("1 document updated");
+                res.send("true");
+            });
+        });
+    });
 });
 
 app.delete('/friends/:userid', (req, res) => {
     res.send('Hello friends DELETE : userid:' +  req.params.userid + '. FriendId : ' + req.body.friendid + '. MongoDB Environment Variable : ' + process.env.MONGO_DB);
 });
 
-
-
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening on port 3000!');
+    console.log('inside init initializing db')
+    MongoClient.connect(url, function(err, db) {
+        gDb = db;
+        if (err) throw err;
+        var dbo = db.db("friendsdb");
+        gDbo = dbo;
+        dbo.createCollection("tblFriends", function(err, res) {
+            if (err) throw err;
+            console.log("Collection created!");
+            // var myobj = { userId: 4, userName: 'testUserFour', age: 30, movieType: 'comedy', friends: [] };
+            // dbo.collection("tblFriends").insertOne(myobj, function(err, res) {
+            //   if (err) throw err;
+            //   console.log("1 document inserted");
+            // });
+            //----------------------------------------------------------------------------
+            // dbo.collection("tblFriends").findOne({"userId": 1}, function(err, result) {
+            //     if (err) throw err;
+            //     console.log('fetching')
+            //     console.log(result);
+            // });
+            // db.close();
+        });
+    });
+})
 
 
